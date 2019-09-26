120db698408ceeb818e5a9972956cede1be99a70
Deinyefa <df.eporwei@gmail.com>
2019-09-26 06:42:38 -0400

    search for venues

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -154,15 +154,27 @@ def search_venues():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for Hop should return "The Musical Hop".
     # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-    response = {
-        "count": 1,
-        "data": [{
-            "id": 2,
-            "name": "The Dueling Pianos Bar",
-            "num_upcoming_shows": 0,
-        }]
+
+    search_term = request.form.get('search_term', '')
+    search_query = Venue.query.filter(Venue.name.ilike('%' + search_term + '%')).all()
+   
+    resultsData = []
+
+    venue_shows = Venue.query.join('shows').all()
+
+    for search in search_query:
+        resultsData.append({
+            "id": search.id,
+            "name": search.name,
+            "num_upcoming_shows": len(venue_shows)
+        })
+
+    response  = {
+        "count": len(search_query),
+        "data": resultsData
     }
-    return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+    
+    return render_template('pages/search_venues.html', results=response, search_term=search_term)
 
 
 @app.route('/venues/<int:venue_id>')
