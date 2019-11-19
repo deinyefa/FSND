5d4b2208124bde3e27696665e222f60779b11302
Deinyefa Eporwei <df.eporwei@gmail.com>
2019-11-19 15:29:07 -0500

    added movies and actors to navbar

--- projects/capstone/frontend/src/components/NavBar.js
+++ projects/capstone/frontend/src/components/NavBar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useAuth0 } from '../react-auth0-spa';
-import {Button} from 'reactstrap'
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
     return (
         <>
+          <Link to='/movies'>Movies</Link>
+          <Link to='/actors'>Actors</Link>
           {!isAuthenticated && (
             <Button onClick={() => window.location.replace(loginWithRedirect())}>Log in</Button>
           )}
